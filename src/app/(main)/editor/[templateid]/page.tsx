16765079569db16c44db31168cb0cd64a868cc1d
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-02-13 22:19:35 -0300

    feat: editor header

---
+++ src/app/(main)/editor/[templateid]/page.tsx
@@ -0,0 +1,12 @@
+import EditorHeader from "@/components/custom/EditorHeader";
+import React from "react";
+
+const Editor = () => {
+  return (
+    <div>
+      <EditorHeader />
+    </div>
+  );
+};
+
+export default Editor;
---
+++ src/components/custom/EditorHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { Code, Monitor, Smartphone } from "lucide-react";
+
+const EditorHeader = () => {
+  return (
+    <div className="p-4 shadow-sm flex justify-between items-center">
+      <Image src={"/logo.svg"} alt="logo" width={160} height={160} />
+      <div>
+        <Button variant="ghost">
+          <Monitor /> Desktop
+        </Button>
+        <Button variant="ghost">
+          <Smartphone /> Mobile
+        </Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="ghost" className="hover:text-primary">
+          <Code />
+        </Button>
+        <Button variant="outline">Send Test Email</Button>
+        <Button>Save Template</Button>
+      </div>
+    </div>
+  );
+};
+
+export default EditorHeader;
