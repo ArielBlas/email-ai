6370d7b69fd7813e16e970a0113061613397b2bd
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-03-03 23:11:29 -0300

    feat: delete layout option

--- src/components/LayoutElements/ColumnLayout.tsx
+++ src/components/LayoutElements/ColumnLayout.tsx
@@ -11,6 +11,7 @@ import TextComponent from "../custom/Element/TextComponent";
 import ImageComponent from "../custom/Element/ImageComponent";
 import LogoComponent from "../custom/Element/LogoComponent";
 import DividerComponent from "../custom/Element/DividerComponent";
+import { Trash } from "lucide-react";
 
 const ColumnLayout = ({ layout }) => {
   const [dragOver, setDragOver] = useState(false);
@@ -55,14 +56,23 @@ const ColumnLayout = ({ layout }) => {
     }
   };
 
+  const deleteLayout = (layoutId) => {
+    const updateEmailTemplate = emailTemplate.filter(
+      (item) => item.id !== layoutId
+    );
+    setEmailTemplate(updateEmailTemplate);
+    setSelectedElement(null);
+  };
+
   return (
-    <div>
+    <div className="relative">
       <div
         style={{
           display: "grid",
           gridTemplateColumns: `repeat(${layout.numOfCol}, 1fr)`,
           gap: "0px",
         }}
+        className={`${selectedELement?.layout?.id == layout.id && "border border-dashed border-blue-500"}`}
       >
         {Array.from({ length: layout.numOfCol }).map((_, index) => (
           <div
@@ -70,7 +80,7 @@ const ColumnLayout = ({ layout }) => {
             className={`p-0 flex items-center h-full w-full bg-white cursor-pointer
               ${!layout?.[index]?.type && "bg-gray-100 border border-dashed"} justify-center
               ${index === dragOver?.index && dragOver?.columnId ? "bg-green-100" : "bg-gray-100"}
-              ${selectedELement?.layout?.id == layout.id && selectedELement?.index == index && "border-blue-500 border-2xs"}
+              ${selectedELement?.layout?.id == layout.id && selectedELement?.index == index && "border-blue-500 border-4"}
               `}
             onDragOver={(event) => onDragOverHandler(event, index)}
             onDrop={onDropHandler}
@@ -79,6 +89,15 @@ const ColumnLayout = ({ layout }) => {
             {GetElementComponent(layout?.[index]) ?? "Drag Element Here"}
           </div>
         ))}
+
+        {selectedELement?.layout?.id == layout.id && (
+          <div
+            className="absolute -right-10 cursor-pointer bg-gray-100 p-2 rounded-full hover:scale-105 transition-all hover:shadow-md"
+            onClick={() => deleteLayout(layout.id)}
+          >
+            <Trash className="w-4 h-4 text-red-500" />
+          </div>
+        )}
       </div>
     </div>
   );
