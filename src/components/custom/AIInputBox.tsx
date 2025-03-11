408d2f8a1af5959f746a832d9f468979a6f878e6
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-03-10 23:54:00 -0300

    feat: save template api

--- src/components/custom/AIInputBox.tsx
+++ src/components/custom/AIInputBox.tsx
@@ -1,23 +1,36 @@
 "use client";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import Prompt from "@/Data/Prompt";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { v4 as uuidv4 } from "uuid";
+import { useUserDetail } from "@/app/provider";
 
 const AIInputBox = () => {
   const [userInput, setUserInput] = useState("");
   const [loading, setLoading] = useState(false);
+  const SaveTemplate = useMutation(api.emailTemplate.SaveTemplate);
+  const { userDetail } = useUserDetail();
 
   const onGenerate = async () => {
     const PROMPT = Prompt.EMAIL_PROMPT + "\n" + userInput;
+    const tid = uuidv4();
     setLoading(true);
 
     try {
-      const res = await fetch("/api/ai-email.generate", {
+      const result = await fetch("/api/ai-email.generate", {
         method: "POST",
         body: {
           prompt: PROMPT,
         },
+      });
+      await SaveTemplate({
+        id: tid,
+        design: result.data,
+        prompt: PROMPT,
+        email: userDetail.email,
       });
       console.log(res);
     } catch (e) {
