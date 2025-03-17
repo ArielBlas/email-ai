ff5d942b46753412d3f3e39c109cc88c231d548d
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-03-16 23:26:41 -0300

    feat: email template card

--- src/components/custom/EmailTemplateList.tsx
+++ src/components/custom/EmailTemplateList.tsx
@@ -25,12 +25,31 @@ const EmailTemplateList = () => {
   return (
     <div>
       <h2 className="font-bold text-xl text-primary mt-6">Workspace</h2>
-      {emailList?.length && (
+      {emailList?.length ? (
         <div className="flex justify-between mt-7 flex-col items-center">
           <Image src={"/email.png"} alt="email" width={260} height={260} />
           <Link href={"/dashboard/create"}>
             <Button className="mt-7">+ Create New</Button>
           </Link>
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5">
+          {emailList.map((item, index) => (
+            <div key={index} className="p-5 rounded-lg shadow-md border">
+              <Image
+                src="/emailbox.png"
+                alt="email"
+                width={200}
+                height={200}
+                className="w-full"
+              />
+
+              <h2 className="mt-2">{item?.description}</h2>
+              <Link href={`/editor/${item?.tid}`}>
+                <Button className="mt-2 w-full">View/Edit</Button>
+              </Link>
+            </div>
+          ))}
         </div>
       )}
     </div>
